f1ca1274aa50c4608487958ccc3e45ffd90803d8
arishamays1 <1arishamays1@gmail.com>
2022-04-25 18:05:44 -0400

    server.js connected to MONGO

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express= require('express')
+const methodOverride = require('method-override')
 const app= express()
 PORT= 4000
+ // db connection
+require('./config/db.connection')
+
+//---------Middleware---------
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.set('view engine', 'ejs')
 
 
+
+//-----------Routing-----------
 app.get('/', (req, res)=>{
     res.send(`This works!`)
 })
