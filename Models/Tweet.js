4da2bf86fab559ac9ff4dd8c703ec35c072c2cf7
kbaldeoward <kbaldeoward@gmail.com>
2022-04-27 13:17:16 -0400

    updates to tweet schema and progress on like function

--- Models/Tweet.js
+++ Models/Tweet.js
@@ -4,18 +4,18 @@ const tweetSchema = new mongoose.Schema({
     handle: { 
         type: String, 
         required: [true, 'you need a Twitter handle']
-
     },
     text: {
         type: String,
         required: [true, 'you need text']
     },
-    image: String,
-    }, 
-    {
-        timestamps: true
-    }
-)
+    image: String, 
+    tweetLikes: {
+        type: Number,
+        default: 0
+    },
+    // timestamps: true
+})
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
