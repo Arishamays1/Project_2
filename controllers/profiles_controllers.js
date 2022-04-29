c4a318e6604269c6672eb32cfdf6bb75cf8c024d
kbaldeoward <kbaldeoward@gmail.com>
2022-04-29 12:06:19 -0400

    can like tweets in profile

--- controllers/profiles_controllers.js
+++ controllers/profiles_controllers.js
@@ -3,11 +3,12 @@ const { Profile } = require('../models');
 const router = express.Router();
 const db = require('../models');
 
+let foundProfile;
 
 //------SHOW PROFILE ROUTE----------
 router.get('/:id', async (req, res, next) => {
     try {
-    const foundProfile = await db.Profile.findById(req.params.id)
+    foundProfile = await db.Profile.findById(req.params.id)
     console.log(foundProfile)
     const foundTweets = await db.Tweet.find({handle: req.params.id})
     console.log(foundTweets)
@@ -28,5 +29,24 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+// route for likes on profile page
+router.post('/liked/:id', async (req, res, next) => {
+    try {
+        const tweet = await db.Tweet.findById(req.params.id);
+        if (tweet) {
+            tweet.tweetLikes += 1;
+            await db.Tweet.findByIdAndUpdate(req.params.id, {tweetLikes: tweet.tweetLikes});
+            res.redirect(`/profiles/${foundProfile._id}`);
+        }
+        else {
+            // Throw 404 Tweet Not Found
+        }
+    }
+    catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
 
 module.exports = router;
